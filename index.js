800773b90308de30efcd572af9126f0bbd388f15
Harry Kent <kenth3@coventry.ac.uk>
2019-11-23 15:19:18 +0000

    Implementing isolated acceptance tests

--- index.js
+++ index.js
@@ -31,6 +31,8 @@ const defaultPort = 8080
 const port = process.env.PORT || defaultPort
 const dbName = 'website.db'
 
+let user
+
 /**
  * The secure home page.
  *
@@ -69,13 +71,15 @@ router.post('/register', koaBody, async ctx => {
 		const body = ctx.request.body
 		console.log(body)
 		// call the functions in the module
-		const user = await new User(dbName)
+		user = await new User(dbName)
 		await user.register(body.user, body.pass)
 		// await user.uploadPicture(path, type)
 		// redirect to the home page
 		ctx.redirect(`/?msg=new user "${body.name}" added`)
 	} catch(err) {
 		await ctx.render('error', {message: err.message})
+	} finally {
+		user.tearDown()
 	}
 })
 
@@ -89,12 +93,14 @@ router.get('/login', async ctx => {
 router.post('/login', async ctx => {
 	try {
 		const body = ctx.request.body
-		const user = await new User(dbName)
+		user = await new User(dbName)
 		await user.login(body.user, body.pass)
 		ctx.session.authorised = true
 		return ctx.redirect('/?msg=you are now logged in...')
 	} catch(err) {
 		await ctx.render('error', {message: err.message})
+	} finally {
+		user.tearDown()
 	}
 })
 
---
+++ jest.puppeteer.config.js
@@ -0,0 +1,14 @@
+
+'use strict'
+
+module.exports = {
+	server: {
+		command: 'node index.js',
+		port: 8080,
+	},
+	launch: {
+		headless: false,
+		devtools: true,
+		timeout: 30000
+	}
+}
