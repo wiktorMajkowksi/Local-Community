4737d1ce7551d6d85450fa88b6fbe6cbc6743ae4
Mark Tyers <aa7401@coventry.ac.uk>
2019-11-23 19:44:19 +0000

    Limit Test Scripts to Directory

--- acceptanceTests/scripts/test.sh
+++ acceptanceTests/scripts/test.sh
@@ -6,6 +6,6 @@ mkdir -p screenshots
 mkdir -p trace
 # [ ! -d "node_modules" ] && echo "INSTALLING MODULES" && npm install
 node index.js&
-node_modules/.bin/jest --runInBand --detectOpenHandles
+node_modules/.bin/jest --runInBand --detectOpenHandles acceptanceTests/*
 read -p "Press enter to continue"
 kill %1
